f70e8b959807408f0bb2cfdaf8b80e0fd5d4bfbc
Abhishek Sengupta <abhishek.sengupta047@gmail.com>
2024-03-31 12:08:56 +0530

    CountSubArrayWithFixedBounds

---
+++ src/main/java/revisit/array/CountSubArrayWithFixedBounds.java
@@ -0,0 +1,24 @@
+package revisit.array;
+
+public class CountSubArrayWithFixedBounds {
+
+    //https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/
+
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        int minI = -1, maxI = -1, left = -1, right = 0;
+        long count = 0;
+        while (right < nums.length) {
+            if (nums[right] < minK || nums[right] > maxK) {
+                minI = right;
+                maxI = right;
+                left = right;
+            }
+            minI = nums[right] == minK ? right : minI;
+            maxI = nums[right] == maxK ? right : maxI;
+            count += Math.min(minI, maxI) - left;
+            right++;
+        }
+        return count;
+    }
+
+}
